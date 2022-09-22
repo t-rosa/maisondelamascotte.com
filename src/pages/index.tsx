aa19fb2522dcd17a4ba93613cbd6e3de74594026
Toma Rosa <toma.rosa@protonmail.com>
2022-09-22 16:33:01 +0200

    Edit images and text sizes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/future/image";
+import Head from "next/head";
 import Link from "next/link";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
-import clsx from "clsx";
 
 const Home: NextPage = () => {
   return (
@@ -22,14 +19,12 @@ const Home: NextPage = () => {
         >
           <header className="col-[2/3] row-[1/2]">
             <nav className="grid place-items-center w-full h-full">
-              <Link href="/" passHref>
-                <a className="text-3xl md:text-4xl border-b border-gray-400">
-                  Maison de la mascotte
-                </a>
-              </Link>
+              <h2 className="text-3xl sm:text-4xl md:text-5xl border-b border-gray-400">
+                Maison de la mascotte
+              </h2>
             </nav>
           </header>
-          <div className="col-[2/3] row-[2/3] bg-[url('/hero.webp')] bg-cover bg-no-repeat bg-fixed bg-center border border-gray-300">
+          <div className="col-[2/3] row-[2/3] bg-[url('/hero.webp')] bg-cover bg-no-repeat  bg-center border border-gray-300">
             <div className="grid place-items-center h-full">
               <div className="grid place-items-center gap-9">
                 <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl text-gray-800">
@@ -50,31 +45,31 @@ const Home: NextPage = () => {
         </section>
         <section
           id="discover"
-          className="grid grid-rows-[5rem_auto_20rem] grid-cols-[1rem_auto_1rem] sm:grid-rows-[5rem_auto_20rem] sm:grid-cols-[5rem_auto_5rem]"
+          className="grid grid-rows-[5rem_auto_15rem] grid-cols-[1rem_auto_1rem] sm:grid-rows-[5rem_auto_15rem] sm:grid-cols-[5rem_auto_5rem]"
         >
-          <header className="col-[2/3] row-[1/2]">
+          <header className="col-[2/3] row-[1/2] pb-16">
             <nav className="grid place-items-center w-full h-full">
               <Link href="/" passHref>
-                <a className="text-3xl md:text-4xl border-b border-gray-400">
+                <a className="text-3xl sm:text-4-xl md:text-5xl border-b border-gray-400">
                   Gallerie
                 </a>
               </Link>
             </nav>
           </header>
-          <div className="col-[2/3] row-[2/3] w-fit gap-24 justify-self-center mt-24 flex flex-col lg:grid lg:grid-cols-2 xl:grid-cols-3 place-items-center">
-            <div className="border-2 shadow-lg relative w-[24rem] h-[24rem] hover:scale-105 duration-300 hover:border-indigo-500 hover:shadow-xl">
+          <div className="col-[2/3] row-[2/3] w-fit gap-24 justify-self-center flex flex-col lg:grid lg:grid-cols-2 xl:grid-cols-3 place-items-center">
+            <div className="border-2 shadow-lg relative w-72 sm:w-96 h-96 hover:scale-105 duration-300 hover:border-indigo-500 hover:shadow-xl">
               <Image src="/image-5.jpg" fill alt="" className="object-cover" />
             </div>
-            <div className="border-2 shadow-lg relative w-[24rem] h-[24rem] hover:scale-105 duration-300 hover:border-indigo-500 hover:shadow-xl">
+            <div className="border-2 shadow-lg relative w-72 sm:w-96 h-96 hover:scale-105 duration-300 hover:border-indigo-500 hover:shadow-xl">
               <Image src="/image-2.jpg" fill alt="" className="object-cover" />
             </div>
-            <div className="border-2 shadow-lg relative w-[24rem] h-[24rem] hover:scale-105 duration-300 hover:border-indigo-500 hover:shadow-xl">
+            <div className="border-2 shadow-lg relative w-72 sm:w-96 h-96 hover:scale-105 duration-300 hover:border-indigo-500 hover:shadow-xl">
               <Image src="/image-1.jpg" fill alt="" className="object-cover" />
             </div>
-            <div className="xl:col-[1/3] xl:row-[2/3] border-2 shadow-lg relative w-[24rem] h-[24rem] hover:scale-105 duration-300 hover:border-indigo-500 hover:shadow-xl">
+            <div className="xl:col-[1/3] xl:row-[2/3] border-2 shadow-lg relative w-72 sm:w-96 h-96 hover:scale-105 duration-300 hover:border-indigo-500 hover:shadow-xl">
               <Image src="/image-3.jpg" fill alt="" className="object-cover" />
             </div>
-            <div className="lg:col-[1/3] xl:row-[2/3] xl:col-[2/4] border-2 shadow-lg relative w-[24rem] h-[24rem] hover:scale-105 duration-300 hover:border-indigo-500 hover:shadow-xl">
+            <div className="lg:col-[1/3] xl:row-[2/3] xl:col-[2/4] border-2 shadow-lg relative w-72 sm:w-96 h-96 hover:scale-105 duration-300 hover:border-indigo-500 hover:shadow-xl">
               <Image src="/image-4.jpg" fill alt="" className="object-cover" />
             </div>
           </div>
@@ -86,7 +81,7 @@ const Home: NextPage = () => {
               className="
                   col-[2/3] row-[3/4] place-self-center
                   text-3xl sm:text-4xl md:text-5xl lg:text-5xl text-gray-800 border border-gray-600 py-2 px-6
-                  hover:scale-105 hover:shadow-lg duration-300 font-bold bg-blue-200 bg-opacity-20 z-10"
+                  hover:scale-105 hover:shadow-lg duration-300 font-bold"
             >
               Détails et réservation
             </a>
