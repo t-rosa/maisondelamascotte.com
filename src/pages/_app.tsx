94dc39ec66886eddb69ae4d772aa333a25d4e4c2
Toma Rosa <toma.rosa@protonmail.com>
2022-09-22 14:53:11 +0200

    Initialization

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import "@fontsource/allura";
+import "../styles/globals.css";
+import type { AppType } from "next/dist/shared/lib/utils";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
