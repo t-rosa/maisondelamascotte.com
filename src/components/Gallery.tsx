f39954d0b7faa75777c659ea9428c36902fe447d
Toma Rosa <toma.rosa@protonmail.com>
2022-10-02 00:08:42 +0200

    Customize animation

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,6 +2,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import * as Icon from "@heroicons/react/24/outline";
 import Image from "next/future/image";
 import { useState } from "react";
+import useMeasure from "react-use-measure";
 
 const images = [
     {
@@ -43,6 +44,7 @@ const images = [
 
 function Gallery() {
     const [imageId, setImageId] = useState(1);
+    const [ref, { width }] = useMeasure();
     const [tuple, setTuple] = useState([1, imageId]);
 
     if (tuple[1] !== imageId && tuple[1]) {
@@ -50,7 +52,7 @@ function Gallery() {
     }
 
     const prev = tuple[0] !== undefined && tuple[0];
-    const direction = imageId > prev ? "next" : "previous";
+    const direction = imageId > prev ? 1 : -1;
 
     function next() {
         setImageId(imageId + 1);
@@ -61,12 +63,12 @@ function Gallery() {
     }
 
     const variants = {
-        enter: (direction: string) => ({
-            x: direction === "next" ? "100%" : "-100%",
+        enter: ({ direction, width }: any) => ({
+            x: direction * width,
         }),
         center: { x: 0 },
-        exit: (direction: string) => ({
-            x: direction === "next" ? "-100%" : "100%",
+        exit: ({ direction, width }: any) => ({
+            x: direction * -width,
         }),
     };
 
@@ -75,19 +77,22 @@ function Gallery() {
             <header className="flex items-center justify-center">
                 <h2 className="sm:text-4-xl border-b border-gray-400 text-3xl md:text-5xl">Galerie</h2>
             </header>
-            <div className="relative grid aspect-square w-[400px] place-items-center place-self-center overflow-hidden border-2 border-indigo-300 bg-zinc-800 sm:w-[550px] md:aspect-video md:w-[700px] lg:w-[900px]">
+            <div
+                ref={ref}
+                className="relative grid aspect-square w-[300px] place-items-center place-self-center overflow-hidden border-2 border-indigo-300 bg-zinc-800 sm:w-[550px] md:aspect-video md:w-[600px] lg:w-[900px]"
+            >
                 <button disabled={imageId === 1} onClick={previous} className="absolute left-0 z-10 h-full bg-gray-300 bg-opacity-20">
                     <Icon.ChevronLeftIcon className="h-10 w-10 text-white sm:h-16 sm:w-16 md:h-20 md:w-20" strokeWidth={1} />
                 </button>
-                <AnimatePresence custom={direction}>
+                <AnimatePresence custom={{ direction, width }}>
                     <motion.img
                         key={imageId}
                         variants={variants}
-                        custom={direction}
+                        custom={{ direction, width }}
                         initial="enter"
                         animate="center"
                         exit="exit"
-                        // transition={{ duration: 1 }}
+                        transition={{ type: "tween", duration: 0.3 }}
                         src={`/image-${imageId}.webp`}
                         className="absolute aspect-square object-cover md:aspect-video"
                         alt={`Image n°${imageId}`}
